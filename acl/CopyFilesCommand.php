909a0100c1f48a72f23b7a6a3c386dbdf4125837
Ribamar FS <ribafs@gmail.com>
2020-10-01 19:53:16 -0300

    Update

--- acl/CopyFilesCommand.php
+++ acl/CopyFilesCommand.php
@@ -29,22 +29,22 @@ class CopyFilesCommand extends Command
     {
         $seeder = base_path('database/seeders/DatabaseSeeder.php');
         if(File::exists($seeder)){
-            File::copy($seeder), base_path('database/seeders/DatabaseSeederBAK.php'));
+            File::copy($seeder, base_path('database/seeders/DatabaseSeederBAK.php'));
             File::copy(base_path('vendor/ribafs/laravel-acl/acl/seeders/DatabaseSeeder.php'), base_path('database/seeders/DatabaseSeeder.php'));
         }
         $route = base_path('routes/web.php');
         if(File::exists($route)){
-            File::copy($route), base_path('routes/webBAK.php');
+            File::copy($route, base_path('routes/webBAK.php'));
             File::copy(base_path('vendor/ribafs/laravel-acl/acl/web.php'), base_path('routes/web.php'));
         }
         $wel = base_path('resources/views/welcome.blade.php');
         if(File::exists($wel)){
-            File::copy($wel), base_path('resources/views/welcome.bladeBAK.php');
+            File::copy($wel, base_path('resources/views/welcome.bladeBAK.php'));
             File::copy(base_path('vendor/ribafs/laravel-acl/acl/views/welcome.blade.php'), base_path('resources/views/welcome.blade.php'));
         }
-        $app = base_path('resources/views/welcome.blade.php')
+        $app = base_path('resources/views/layouts/app.blade.php');
         if(File::exists($app)){
-            File::copy($wel), base_path('resources/views/welcome.bladeBAK.php');
+            File::copy($app, base_path('resources/views/layouts/app.bladeBAK.php'));
             File::copy(base_path('vendor/ribafs/laravel-acl/acl/views/layouts/app.blade.php'), base_path('resources/views/layouts/app.blade.php'));
         }
 
